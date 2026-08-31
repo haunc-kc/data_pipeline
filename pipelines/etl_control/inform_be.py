7bdbe2c2c9c519d685fc05e71ee419b90f965252
Hau Nguyen <hau.nguyen@kosatec.vn>
2026-08-31 18:25:19 +0700

    add schedule only for production and test filter on sales invoice header

--- pipelines/etl_control/inform_be.py
+++ pipelines/etl_control/inform_be.py
@@ -1,2 +1,4 @@
 spark.sql("INSERT INTO dw_mention_serving.etl_inform (date_create) VALUES (current_timestamp())")
+print("Done With UAT")
 spark.sql("INSERT INTO dw_mention_serving.etl_inform_prd (date_create) VALUES (current_timestamp())")
+print("Done With PROD")
--- pipelines/facts/jobs/fact_sales_invoice_master.py
+++ pipelines/facts/jobs/fact_sales_invoice_master.py
@@ -252,7 +252,8 @@ _HEADER_SET_COLS = (["sk_customer_id", "sk_salesperson_id"]  + _HEADER_HASH_COLS
 try:
     DeltaTable.forName(spark, SOURCE_HEADER_TABLE).alias("t").merge(
         header_df.alias("s"),
-        ("t.nk_document_id = s.nk_document_id "
+        ("t.document_creation_date >= {dateControl}"
+        "And t.nk_document_id = s.nk_document_id "
         " And t.nk_document_no = s.nk_document_no "
         " And t.sk_customer_id = s.sk_customer_id"
         " And t.sk_salesperson_id = s.sk_salesperson_id"
