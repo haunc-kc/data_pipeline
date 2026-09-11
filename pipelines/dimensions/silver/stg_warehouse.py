d78099158e1651d3ddad189b7f5a24addd766eda
Hau Nguyen <hau.nguyen@kosatec.vn>
2026-09-11 15:38:33 +0700

    disable scheduler

--- pipelines/dimensions/silver/stg_warehouse.py
+++ pipelines/dimensions/silver/stg_warehouse.py
@@ -23,7 +23,7 @@ def build_stg_warehouse():
                             r"(.{8})(.{4})(.{4})(.{4})(.{12})",
                             r"$1-$2-$3-$4-$5"
                             ).alias("sk_warehouse_id"),
-                            F.col(" ").alias("nk_warehouse_id")
+                            F.col("MLIDNR").alias("nk_warehouse_id")
                             ,F.col("MLLAGER").alias("warehouse_name"),
                             F.col("MLEKPREIS").alias("purchase_price_avg"),
                             F.col("MLEKPREP").alias("purchase_price_repair"),
